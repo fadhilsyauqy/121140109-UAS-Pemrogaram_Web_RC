4d743db338b1b786e1da4d7f9113a37506c1781c
Fadhil Syauqy <rahmatfadhilsauqy08@gmail.com>
2023-12-17 19:08:53 +0700

    update js

--- script/validasi_login.js
+++ script/validasi_login.js
@@ -1,11 +1,9 @@
-function validateRegis() {
+function validateLogin() {
     var email = document.getElementById('email').value.trim();
     var password = document.getElementById('password').value.trim();
-    var password2 = document.getElementById('password2').value.trim();
 
     var emailError = document.getElementById('emailError');
     var passwordError = document.getElementById('passwordError');
-    var password2Error = document.getElementById('password2Error');
 
     // Validasi Email
     if (email === '') {
@@ -23,17 +21,8 @@ function validateRegis() {
         passwordError.innerHTML = '';
     }
 
-    // Validasi Konfirmasi Password
-    if (password2 === '') {
-        password2Error.innerHTML = 'Konfirmasi Password harus diisi';
-    } else if (password !== password2) {
-        password2Error.innerHTML = 'Konfirmasi Password tidak cocok dengan Password';
-    } else {
-        password2Error.innerHTML = '';
-    }
-
-    // Jika ada setidaknya satu pesan error, form tidak akan dikirim
-    if (email === '' || password === '' || password2 === '' || !validateEmail(email) || password !== password2) {
+    // Jika tidak ada pesan error, form akan dikirim
+    if (email === '' || password === '' || !validateEmail(email)) {
         return false; // Prevent form submission
     }
     return true; // Allow form submission
